d21f7dc5fa89ab1c82006db784e031d82215899c
Matt Koskela <mattkoskela@gmail.com>
2018-03-14 21:42:34 -0700

    Formatting cleanup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 //const API = 'http://niv84.com/bibleapi.php?url=https://bibles.org/v2/verses/eng-GNTD:Acts.8.34.js';
 //const APIKEY = '&key=wfZdpay5cJlTlo1kFLqdHB288LLghEZjKk9vcihP';
-const API = 'http://bible-api.com/john%203:16';
+const API = 'https://bible-api.com/john%203:16';
 const APIKEY = '';
 
 class App extends React.Component {
@@ -13,43 +13,44 @@ class App extends React.Component {
     this.state = {
       reference: {},
       verse: {},
-      isLoading: false,
+      isLoading: true,
       error: null
     };
   }
 
   componentDidMount() {
-    this.setState({ isLoading: true });
     const params = {
       headers: {
-        "Content-Type": "application/json"}
-    }
+        "Content-Type": "application/json"
+      }
+    };
     axios.get(API + APIKEY, params)
-          .then((response) => {
-            console.log(response);
-            console.log(response.data.reference);
-            this.setState({
-              reference: response.data.reference,
-              verse: response.data.text, 
-              isLoading: false})
-          }).catch((error) => {
-            console.log('Error calling axios (get): ', error);
-          })
+      .then((response) => {
+        console.log(response);
+        console.log(response.data.reference);
+        this.setState({
+          reference: response.data.reference,
+          verse: response.data.text,
+          isLoading: false})
+      }).catch((error) => {
+        console.log('Error calling axios (get): ', error);
+      })
   }
 
   render() {
-  return (
-    <div>
-      { (this.state.isLoading === true) ? (
-        <div>Loading...</div>
-      ) : (
-        <div>
-          Reference: {this.state.reference}
-          Verse: {this.state.verse}
-        </div>
-      )}
-    </div>
-  )
-}
+    return (
+      <div>
+        { (this.state.isLoading === true) ? (
+          <div>Loading...</div>
+        ) : (
+          <div>
+            Reference: {this.state.reference}
+            Verse: {this.state.verse}
+          </div>
+        )}
+      </div>
+    );
+  }
 };
+
 export default App;
